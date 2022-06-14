8ae347bafe161502d6bee22827aa348de5e54805
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-14 12:01:50 +0300

    additions

--- ObjectWithCode/ViewController.swift
+++ ObjectWithCode/ViewController.swift
@@ -8,15 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var  myLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let width = view.frame.size.width
         let height = view.frame.size.height
         
-        let myLabel = UILabel()
+        
         myLabel.text = "Test Label"
         myLabel.textAlignment = .center
         //Eğer bir hata veriyorsa commandB ile build ediniz.
@@ -25,11 +26,22 @@ class ViewController: UIViewController {
          
         //storyboard yerine code ile görünüm vermemizdeki amaç her ekrana uygun olmasını sağlamaktır.
         
+        //Button Görünüm Ayarları
+        let myButton = UIButton()
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
         
+        myButton.frame = CGRect(x:width * 0.5 - 100, y:height * 0.6, width: 200, height: 100)
+        view.addSubview(myButton)
         
+        //Buton Tıklanma Ayarları
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside)
         
     }
-
-
+    @objc func myAction(){
+        myLabel.text = "Tapped"
+    }
+ //Not: Kompleks projelerde profesyonel ios geliştirici şirketleri storyboard yerine code ile görünümler oluşturmayı tercih ediyorlardı fakat zamanla bu yerini SwiftUI'a bıkaracak gibi görünüyor.
+//Happy Coding y'all :)
 }
 
